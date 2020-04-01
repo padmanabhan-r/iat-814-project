722370918d196952872f73a3a238461b63c7a686
Padmanabhan Rajendrakumar <padmanabhan.rajendrakumar@gmail.com>
2020-04-01 15:58:53 -0700

    V3

---
+++ Use this V3/src/components/turnaround/turnhr.js
@@ -0,0 +1,358 @@
+import React, {PureComponent} from 'react';
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  ZAxis,
+  Tooltip,
+  Legend,
+} from 'recharts';
+import data from '../../data/turnar';
+
+const [unit1E, unit1N, unit1S, unit1W, unitBP, unitSCU] = data;
+
+const parseDomain = () => [
+  0,
+  Math.max(
+    Math.max.apply(
+      null,
+      unit1E.map((entry) => entry.value)
+    ),
+    Math.max.apply(
+      null,
+      unit1S.map((entry) => entry.value)
+    ),
+    Math.max.apply(
+      null,
+      unit1W.map((entry) => entry.value)
+    ),
+    Math.max.apply(
+      null,
+      unit1N.map((entry) => entry.value)
+    ),
+    Math.max.apply(
+      null,
+      unitBP.map((entry) => entry.value)
+    ),
+    Math.max.apply(
+      null,
+      unitSCU.map((entry) => entry.value)
+    )
+  ),
+];
+
+class TurnHr extends PureComponent {
+  renderTooltip = (props) => {
+    const {active, payload} = props;
+
+    if (active && payload && payload.length) {
+      const data = payload[0] && payload[0].payload;
+
+      return (
+        <div
+          style={{
+            backgroundColor: '#fff',
+            border: '1px solid #999',
+            margin: 0,
+            padding: 10,
+          }}
+        >
+          <p>{data.hour}</p>
+          <p>
+            <span>Turnaround Time(mins): {data.value}</span>
+          </p>
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  render() {
+    const domain = parseDomain();
+    const range = [0, 1000];
+
+    return (
+      <div>
+        <ScatterChart
+          width={900}
+          height={80}
+          margin={{
+            top: 20,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }}
+        >
+          <XAxis
+            type="category"
+            dataKey="hour"
+            name="hour"
+            interval={0}
+            tick={{fontSize: 0}}
+            tickLine={{transform: 'translate(0, -6)'}}
+          />
+          <YAxis
+            type="number"
+            dataKey="index"
+            height={10}
+            width={80}
+            tick={false}
+            tickLine={false}
+            axisLine={false}
+            label={{
+              value: 'Unit 1E',
+              position: 'insideRight',
+              fontFamily: 'archia',
+            }}
+          />
+          <ZAxis type="number" dataKey="value" domain={domain} range={range} />
+          <Tooltip
+            cursor={{strokeDasharray: '3 3'}}
+            wrapperStyle={{zIndex: 100}}
+            content={this.renderTooltip}
+          />
+          <Scatter
+            data={unit1E}
+            fill="red"
+            shape="circle"
+            line={{stroke: 'black', strokeWidth: 1}}
+          />
+        </ScatterChart>
+
+        <ScatterChart
+          width={900}
+          height={80}
+          margin={{
+            top: 20,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }}
+        >
+          <XAxis
+            type="category"
+            dataKey="hour"
+            name="hour"
+            interval={0}
+            tick={{fontSize: 0}}
+            tickLine={{transform: 'translate(0, -6)'}}
+          />
+          <YAxis
+            type="number"
+            dataKey="index"
+            height={10}
+            width={80}
+            tick={false}
+            tickLine={false}
+            axisLine={false}
+            label={{
+              value: 'Unit 1S',
+              position: 'insideRight',
+              fontFamily: 'archia',
+            }}
+          />
+          <ZAxis type="number" dataKey="value" domain={domain} range={range} />
+          <Tooltip
+            cursor={{strokeDasharray: '3 3'}}
+            wrapperStyle={{zIndex: 100}}
+            content={this.renderTooltip}
+          />
+          <Scatter
+            data={unit1S}
+            fill="red"
+            shape="circle"
+            line={{stroke: 'black', strokeWidth: 1}}
+          />
+        </ScatterChart>
+
+        <ScatterChart
+          width={900}
+          height={80}
+          margin={{
+            top: 20,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }}
+        >
+          <XAxis
+            type="category"
+            dataKey="hour"
+            name="hour"
+            interval={0}
+            tick={{fontSize: 0}}
+            tickLine={{transform: 'translate(0, -6)'}}
+          />
+          <YAxis
+            type="number"
+            dataKey="index"
+            height={10}
+            width={80}
+            tick={false}
+            tickLine={false}
+            axisLine={false}
+            label={{
+              value: 'Unit 1W',
+              position: 'insideRight',
+              fontFamily: 'archia',
+            }}
+          />
+          <ZAxis type="number" dataKey="value" domain={domain} range={range} />
+          <Tooltip
+            cursor={{strokeDasharray: '3 3'}}
+            wrapperStyle={{zIndex: 100}}
+            content={this.renderTooltip}
+          />
+          <Scatter
+            data={unit1W}
+            fill="red"
+            shape="circle"
+            line={{stroke: 'black', strokeWidth: 1}}
+          />
+        </ScatterChart>
+
+        <ScatterChart
+          width={900}
+          height={80}
+          margin={{
+            top: 20,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }}
+        >
+          <XAxis
+            type="category"
+            dataKey="hour"
+            name="hour"
+            interval={0}
+            tick={{fontSize: 0}}
+            tickLine={{transform: 'translate(0, -6)'}}
+          />
+          <YAxis
+            type="number"
+            dataKey="index"
+            height={10}
+            width={80}
+            tick={false}
+            tickLine={false}
+            axisLine={false}
+            label={{
+              value: 'Unit 1N',
+              position: 'insideRight',
+              fontFamily: 'archia',
+            }}
+          />
+          <ZAxis type="number" dataKey="value" domain={domain} range={range} />
+          <Tooltip
+            cursor={{strokeDasharray: '3 3'}}
+            wrapperStyle={{zIndex: 100}}
+            content={this.renderTooltip}
+          />
+          <Scatter
+            data={unit1N}
+            fill="red"
+            shape="circle"
+            line={{stroke: 'black', strokeWidth: 1}}
+          />
+        </ScatterChart>
+
+        <ScatterChart
+          width={900}
+          height={80}
+          margin={{
+            top: 20,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }}
+        >
+          <XAxis
+            type="category"
+            dataKey="hour"
+            name="hour"
+            interval={0}
+            tick={{fontSize: 0}}
+            tickLine={{transform: 'translate(0, -6)'}}
+          />
+          <YAxis
+            type="number"
+            dataKey="index"
+            height={10}
+            width={80}
+            tick={false}
+            tickLine={false}
+            axisLine={false}
+            label={{
+              value: 'Unit BP',
+              position: 'insideRight',
+              fontFamily: 'archia',
+            }}
+          />
+          <ZAxis type="number" dataKey="value" domain={domain} range={range} />
+          <Tooltip
+            cursor={{strokeDasharray: '3 3'}}
+            wrapperStyle={{zIndex: 100}}
+            content={this.renderTooltip}
+          />
+          <Scatter
+            data={unitBP}
+            fill="red"
+            shape="circle"
+            line={{stroke: 'black', strokeWidth: 1}}
+          />
+        </ScatterChart>
+
+        <ScatterChart
+          width={900}
+          height={80}
+          margin={{
+            top: 20,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }}
+        >
+          <XAxis
+            type="category"
+            dataKey="hour"
+            name="hour"
+            interval={0}
+            tick={{fontSize: 12, fontFamily: 'archia'}}
+            tickLine={{transform: 'translate(0, -6)'}}
+          />
+          <YAxis
+            type="number"
+            dataKey="index"
+            height={10}
+            width={80}
+            tick={false}
+            tickLine={false}
+            axisLine={false}
+            label={{
+              value: 'Unit SCU',
+              position: 'insideRight',
+              fontFamily: 'archia',
+            }}
+          />
+          <ZAxis type="number" dataKey="value" domain={domain} range={range} />
+          <Tooltip
+            cursor={{strokeDasharray: '3 3'}}
+            wrapperStyle={{zIndex: 100}}
+            content={this.renderTooltip}
+          />
+          <Scatter
+            data={unitSCU}
+            fill="red"
+            shape="circle"
+            line={{stroke: 'black', strokeWidth: 1}}
+          />
+        </ScatterChart>
+      </div>
+    );
+  }
+}
+
+export default TurnHr;
